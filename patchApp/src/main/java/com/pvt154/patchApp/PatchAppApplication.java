42c35c88bc47150240fdf1be022f882d12f6257f
ro02in <158111813+ro02in@users.noreply.github.com>
2025-04-03 18:42:21 +0200

    Add files via upload

---
+++ patchApp/src/main/java/com/pvt154/patchApp/PatchAppApplication.java
@@ -0,0 +1,13 @@
+package com.pvt154.patchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatchAppApplication.class, args);
+	}
+
+}
---
+++ patchApp/src/test/java/com/pvt154/patchApp/PatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvt154.patchApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
