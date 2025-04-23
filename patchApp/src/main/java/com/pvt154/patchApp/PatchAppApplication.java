bc478bb36362bbd99c71056631f9f44a480fe3e5
ro02in <robin.karim@live.se>
2025-04-23 18:29:26 +0200

    nu måste det fungera!!

--- patchApp/src/main/java/com/pvt154/patchApp/PatchAppApplication.java
+++ patchApp/src/main/java/com/pvt154/patchApp/PatchAppApplication.java
@@ -2,9 +2,16 @@ package com.pvt154.patchApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PatchAppApplication {
+public class PatchAppApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PatchAppApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatchAppApplication.class, args);
