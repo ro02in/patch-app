bdbd70681d890be2bbfb7c34377ba01fa66c9adc
Alice Kempe <11alicek@gmail.com>
2025-05-14 12:03:41 +0200

    start till trade log sida och trade action sida

---
+++ patchApp/FrontEnd/patchappflutter/lib/trade_action_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TradeActionPage extends StatefulWidget {
+
+  @override
+  State<TradeActionPage> createState() => _TradeActionPageState();
+}
+
+class _TradeActionPageState extends State<TradeActionPage> {
+
+@override
+Widget build (BuildContext context) {
+
+  return Scaffold(
+    backgroundColor: Colors.white,
+    body: Container(
+      
+
+    )
+  );
+}
+}
