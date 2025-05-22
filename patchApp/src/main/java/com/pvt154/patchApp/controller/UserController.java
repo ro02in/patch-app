5210df309444044c62989a09a229b3c447f0a608
ro02in <robin.karim@live.se>
2025-05-22 13:33:17 +0200

    backend för sökning efter users

--- patchApp/src/main/java/com/pvt154/patchApp/controller/UserController.java
+++ patchApp/src/main/java/com/pvt154/patchApp/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.pvt154.patchApp.controller;
 
 import com.pvt154.patchApp.model.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pvt154.patchApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -10,6 +11,16 @@ import java.util.List;
 @RequestMapping(path = "api/user")
 public class UserController {
 
+    private final UserRepository userRepository;
 
+    @Autowired // valfri i nyare Spring, men bra att ha
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
+    // Sökning efter personer
+    @GetMapping("/search")
+    public List<User> searchUsers(@RequestParam("query") String query) {
+        return userRepository.searchUsers(query);
+    }
 }
