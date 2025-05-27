c76dddd135a89c2d20cea50b5763b4ddc1989749
ClydeTheBelovedBandit <nick.khodarahmi@gmail.com>
2025-05-27 15:22:35 +0200

    Changed enum stuff to strings
    Updated everything to make tests work

--- patchApp/src/main/java/com/pvt154/patchApp/model/Patch.java
+++ patchApp/src/main/java/com/pvt154/patchApp/model/Patch.java
@@ -1,16 +1,9 @@
 package com.pvt154.patchApp.model;
 
-import com.pvt154.patchApp.service.PatchCategory;
-import com.pvt154.patchApp.service.PatchColors;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Entity
 @Data
