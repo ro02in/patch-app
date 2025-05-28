251b4aa6742b466f173425321761f2eef2dbf1ac
Alice Kempe <11alicek@gmail.com>
2025-05-28 14:52:08 +0200

    28 maj 14:51 3 filer, ProfilePage, StorePatches ändrade och ny fil patch_model. (Fix med Märkessamling på ProfilePage.)

---
+++ patchApp/FrontEnd/patchappflutter/lib/patch_model.dart
@@ -0,0 +1,53 @@
+import 'dart:ffi';
+import 'dart:typed_data'; //stackoverflow.com/questions/49016508/what-is-the-dart-equivalent-of-java-byte
+
+class PatchModel {
+  final Long patchid;
+  final String description;
+  final String ownerGoogleId;
+  final Uint8List? pictureData;
+  final bool isPublic;
+  final String placement;
+  final String klubbmasteri;
+  final String patchName;
+  final String color;
+
+  PatchModel({
+    required this.patchid,
+    required this.description,
+    required this.ownerGoogleId,
+    required this.pictureData,
+    required this.isPublic,
+    required this.placement,
+    required this.klubbmasteri,
+    required this.patchName,
+    required this.color,
+  });
+
+  factory PatchModel.fromJson(Map<String, dynamic> json) {
+    return PatchModel(
+      patchid: json['patchId'],
+      ownerGoogleId: json['ownerGoogleId'],
+      description: json['description'] ?? '',
+      pictureData: json['pictureData'] ?? '',
+      isPublic: json['isPublic'] ?? '',
+      placement: json['placement'] ?? '',
+      klubbmasteri: json['klubbmasteri'] ?? '',
+      patchName: json['patchName'] ?? '',
+      color: json['color'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() =>
+      {
+        'patchId': patchid,
+        'description': description,
+        'ownerGoogleId': ownerGoogleId,
+        'pictureData': pictureData,
+        'isPublic': isPublic,
+        'placement': placement,
+        'klubbmasteri': klubbmasteri,
+        'patchName': patchName,
+        'color': color,
+      };
+}
