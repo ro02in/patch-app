6e5fedd8c22a7e15cdcb4c38045f299dbaf74515
Alice Kempe <11alicek@gmail.com>
2025-05-30 15:51:38 +0200

    30 maj 15:51: FrontEnd: Fixad main.dart sida design, Continue Log In Page, och Continue Register Page.

--- patchApp/FrontEnd/patchappflutter/lib/Pages/continue_register_page.dart
+++ patchApp/FrontEnd/patchappflutter/lib/Pages/continue_register_page.dart
@@ -1,138 +1,245 @@
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:patchappflutter/Pages/temp_buttons_page.dart';
 
 class ContinueRegisterPage extends StatefulWidget {
   @override
-  State<ContinueRegisterPage> createState() => _ContinueRegisterPageState();
+  _ContinueRegisterPageState createState() => _ContinueRegisterPageState();
 }
 
 class _ContinueRegisterPageState extends State<ContinueRegisterPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> handleLogin() async {
+    final String email = _emailController.text.trim();
+    final String password = _passwordController.text.trim();
+
+    try {
+      final response = await http.post(
+        Uri.parse('https://group-4-15.pvt.dsv.su.se/api/auth/login'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'emailAddress': email,
+          'password': password,
+        }),
+      );
+
+      final responseData = jsonDecode(response.body);
+      if (response.statusCode == 200 && responseData['message'] == 'Login successful.') {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => TempButtonsPage()),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(responseData['message'] ?? 'Login failed')),
+        );
+      }
+    } catch (error) {
+      print('Login error: $error');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Login failed: $error')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
-
     return Scaffold(
       body: Container(
-        width: screenSize.width,
-        height: screenSize.height,
+        alignment: Alignment.center,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/stitches3.png'),
+            image: AssetImage('assets/stitches1.png'),
             fit: BoxFit.fill,
           ),
         ),
         child: ListView(
+          padding: EdgeInsets.only(top: 0),
           children: [
-              SizedBox(
-                  height: 300,
-                  child: Image.asset('assets/rosaovve.png', width: 250, height: 250)
+            AppBar(
+              backgroundColor: Color.fromARGB(170, 34, 42, 22), //Color.fromARGB(255, 243, 92, 255) rosa
+              toolbarHeight: 57,
+              leadingWidth: 160,
+              leading: Icon(Icons.arrow_back, color: Colors.white),
+              titleSpacing: -23,
+              elevation: 10,
+              primary: true,
+              shadowColor: Color.fromARGB(255, 40, 40, 40),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(100),
+                  )
               ),
-            //rad
+            ),
 
-            SizedBox(
-              height: 30,
-              child: Padding(
-                padding: EdgeInsets.only(left: 20, right: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            SizedBox(height: 40),
+
+              Container(
+                width: 200,
+                height: 510,
+                padding: EdgeInsets.only(top: 20, bottom: 20),
+                //color: Color.fromARGB(170, 40, 40, 40),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [Color.fromARGB(170, 40, 40, 40), Color.fromARGB(255, 29, 29, 29)])
+                ),
+                child:
+                Column(
                   children: [
-                    Text(textAlign: TextAlign.end,
-                      "Skapa",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'InknutAntiqua',
-                        fontSize: 19,
-                        fontWeight: FontWeight.w200,
+                    SizedBox(height: 20),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Skapa", style: TextStyle(color: Colors.white, fontFamily: 'InknutAntiqua', fontSize: 20)),
+                          SizedBox(width: 0.4),
+                          Image.asset('assets/syttbyttljusrosatext.png', width: 209, height: 50),
+                          SizedBox(width: 0.4),
+                          Text("konto", style: TextStyle(color: Colors.white, fontFamily: 'InknutAntiqua', fontSize: 20))
+                        ],
                       ),
                     ),
-                      Image.asset(
-                        'assets/syttbyttljusrosatext.png', width: 200, height: 200,
+
+                SizedBox(height: 30),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(26),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 53, 53, 53).withOpacity(0.85),
+                          borderRadius: BorderRadius.circular(40),
+                          border: Border.all(color: Color.fromARGB(255, 70, 70, 70), width: 1.5),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromARGB(255, 30, 30, 30),
+                                spreadRadius: 4,
+                                blurRadius: 20,
+                                offset: Offset(0, 2)
+                            )
+                          ]
                       ),
-                    Text(
-                      textAlign: TextAlign.start,
-                      "konto",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'InknutAntiqua',
-                        fontSize: 19,
-                        fontWeight: FontWeight.w200,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text('Registrera konto', style: TextStyle(color: Colors.white, fontSize: 22, fontFamily: 'InknutAntiqua'),
+                          ),
+                          SizedBox(height: 25),
+
+                          //EMAIL TEXTFIELD
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Color.fromARGB(90, 20, 20, 20),
+                                      spreadRadius: 2,
+                                      blurRadius: 5,
+                                      offset: Offset(0, 2)
+                                  )
+                                ]
+                            ),
+                            child: SizedBox(
+                              height: 55,
+                              width: 260,
+                              child: TextField(
+                                controller: _emailController,
+                                cursorColor: Color.fromARGB(255, 246, 154, 255),
+                                cursorHeight: 22,
+                                decoration: InputDecoration(
+                                  labelText: 'Email',
+                                  labelStyle: TextStyle(color: Colors.white, fontFamily: 'InknutAntiqua', fontSize: 15),
+                                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                                  filled: true,
+                                  fillColor: Colors.white12,
+                                  contentPadding: EdgeInsets.only(top: 5, left: 20, bottom: 5, right: 20),
+                                  border: OutlineInputBorder(),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Color.fromARGB(255, 246, 154, 255), width: 1.5),
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Color.fromARGB(255, 85, 85, 85), width: 1.5),
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                ),
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+
+                          //PASSWORD TEXTFIELD
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Color.fromARGB(90, 20, 20, 20),
+                                      spreadRadius: 2,
+                                      blurRadius: 5,
+                                      offset: Offset(0, 2)
+                                  )
+                                ]
+                            ),
+                            child: SizedBox(
+                              height: 55,
+                              width: 260,
+                              child: TextField(
+                                controller: _passwordController,
+                                cursorColor: Color.fromARGB(255, 246, 154, 255),
+                                cursorHeight: 22,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Password',
+                                  labelStyle: TextStyle(color: Colors.white, fontFamily: 'InknutAntiqua', fontSize: 15),
+                                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                                  filled: true,
+                                  fillColor: Colors.white12,
+                                  contentPadding: EdgeInsets.only(top: 5, left: 20, bottom: 5, right: 20),
+                                  border: OutlineInputBorder(),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Color.fromARGB(255, 246, 154, 255), width: 1.5),
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Color.fromARGB(255, 85, 85, 85), width: 1.5),
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                ),
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: SizedBox(
+                              width: 200,
+                              height: 50,
+                              child: OutlinedButton(
+                                style: OutlinedButton.styleFrom(
+                                    backgroundColor: Color.fromARGB(255, 32, 32, 32),
+                                    side: BorderSide(color: Color.fromARGB(255, 246, 154, 255), width: 1)
+                                ),
+                                onPressed: handleLogin,
+                                child: Text(
+                                    'Skapa konto',
+                                    style: TextStyle(color: Color.fromARGB(255, 230, 230, 230), fontSize: 17, fontFamily: 'InknutAntiqua')),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
+                ],//grå box COLUMN
               ),
-            ),
-
-            SizedBox(height: 18),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 280,
-                  height: 110,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 232, 127, 232),
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromARGB(255, 232, 127, 232),
-                        blurRadius: 10,
-                        spreadRadius: 4,
-                        offset: Offset(0.5, 1),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          // TODO: Lägg till din Google Sign-In-funktion här
-                          print('Google Sign-In tapped');
-                        },
-                        child: Container(
-                          width: 250,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(30),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.1),
-                                blurRadius: 8,
-                                offset: Offset(0, 4),
-                              ),
-                            ],
-                            border: Border.all(color: Colors.grey.shade300),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.asset(
-                                'assets/googlelogorund.png',
-                                height: 30,
-                                width: 30,
-                              ),
-                              SizedBox(width: 12),
-                              Text(
-                                'Fortsätt med Google',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black87,
-                                  fontFamily: 'Roboto',
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
           ],
         ),
       ),
--- patchApp/FrontEnd/patchappflutter/lib/main.dart
+++ patchApp/FrontEnd/patchappflutter/lib/main.dart
@@ -31,41 +31,111 @@ class AuthPage extends StatelessWidget {
         ),
         child: SingleChildScrollView(
           padding: EdgeInsets.symmetric(horizontal: 30),
-          child: Container(
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 53, 53, 53).withOpacity(0.8),
-              borderRadius: BorderRadius.circular(30),
-              border: Border.all(color: Color.fromARGB(255, 226, 93, 246), width: 1),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(255, 39, 39, 39),
-                  blurRadius: 5,
-                  spreadRadius: 2,
-                  offset: Offset(1, 4),
+          child: Column(
+            children: [
+              Text(
+                "WELCOME TO",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 239, 137, 254),
+                  fontFamily: 'InknutAntiqua',
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
                 ),
-              ],
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  "WELCOME TO PATCH APP",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 239, 137, 254),
-                    fontFamily: 'InknutAntiqua',
-                    fontSize: 30,
-                    fontWeight: FontWeight.w400,
-                  ),
+              ),
+
+              SizedBox(height: 6),
+
+              Container(
+                height: 35,
+                width: 300,
+                child: Image.asset('assets/syttbyttljusrosatext.png', height: 450, width: 450),
+              ),
+
+              SizedBox(height: 20),
+
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 53, 53, 53).withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Color.fromARGB(255, 85, 85, 85), width: 1),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromARGB(255, 30, 30, 30),
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                      offset: Offset(1, 4),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 40),
-                buildButton(context, "LOG IN", LoginPage(), Colors.green),
-                SizedBox(height: 20),
-                buildButton(context, "REGISTER", ContinueRegisterPage(), Colors.blue),
-                SizedBox(height: 20),
-                buildButton(context, "DEV MENY", TempButtonsPage(), Colors.orange),
-              ],
-            ),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromARGB(150, 32, 32, 32),
+                                spreadRadius: 3,
+                                blurRadius: 8,
+                                offset: Offset(0, 2)
+                            )
+                          ]
+                      ),
+                      child: Container(
+                        width: 220,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(color: Color.fromARGB(255, 226, 93, 246), width: 1),
+                        ),
+                        child: OutlinedButton(
+                            onPressed: () {
+                              //Gå till LogInPage() med epost och lösenord //30 maj
+                              Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) => LoginPage())); //gå till continue_login_page.dart
+                            },
+                            child: Text("Logga in", style: TextStyle(color: Colors.white, fontFamily: 'InknutAntiqua', fontSize: 17))),
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+
+                    Container(
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromARGB(150, 32, 32, 32),
+                                spreadRadius: 3,
+                                blurRadius: 8,
+                                offset: Offset(0, 2)
+                            )
+                          ]
+                      ),
+                      child: Container(
+                        width: 220,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(color: Colors.purpleAccent, width: 1),
+                        ),
+                        child: OutlinedButton(
+                            onPressed: () {
+                              //Gå till ContinueRegisterPage() med epost och lösenord //30 maj
+                              Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) => ContinueRegisterPage()));
+                            },
+                            child: Text("Registrera konto", style: TextStyle(color: Colors.white, fontFamily: 'InknutAntiqua', fontSize: 17))),
+                      ),
+                    ),
+                   // buildButton(context, "LOG IN", LoginPage(), Colors.purple),
+                    SizedBox(height: 20),
+                   // buildButton(context, "REGISTER", ContinueRegisterPage(), Colors.tealAccent),
+                   // SizedBox(height: 20),
+                    buildButton(context, "DEV MENY", TempButtonsPage(), Colors.orange),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
